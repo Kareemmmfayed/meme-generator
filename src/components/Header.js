69c09959e82d042e6a15adc07a7091f93d1f4057
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2023-08-14 05:13:29 +0300

    added the header and the form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import troll from '../assets/troll-face.png'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className='header'>
+            <img src={troll} alt='troll'/>
+            <h2>Meme Generator</h2>
+            <p> React-Course Project</p>
+        </header>
     )
 }
 
